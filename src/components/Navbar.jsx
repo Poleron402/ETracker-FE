04952690ed034f20d40ae526b6a65d483aafd51e
Polina <zhperon@gmail.com>
2025-04-09 00:03:54 -0700

    maybe temporary fix to expired token

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,49 @@ import Cookies from "js-cookie"
 import { useEffect, useState } from "react"
 import { Button } from "@mui/material"
 import Login from "./Login"
-import { useSelector } from "react-redux"
+import { api, apiError } from "../utilities"
 import Logout from "./Logout"
 
 const Navbar = ()=>{
-    const token = Cookies.get("token")
+    const [authed, setAuthed] = useState(null)
+
+    const checkTokenExpired = async()=>{
+        const token = Cookies.get("token")
+        if (!token){
+            setAuthed(null)
+        }else{
+            setAuthed(token)
+        }
+        try{
+            let response = await api.get("/api/auth/check",{
+                headers:{
+                    Authorization: `Bearer ${token}`
+                }
+            })
+           setAuthed(response.data.token)
+
+        }catch(err){
+            if (err.response?.status === 403){
+                Cookies.remove("token")
+                setAuthed(null)
+            }else{
+                apiError(err, "Checking authentication and token validity")
+            }
+        }
+    }
+    useEffect(()=>{
+        checkTokenExpired()
+    }, [])
+    
+
+
 
     return (
         <>
             <div id="navbar">
                 <Link to="/"><img src={etracker} id='menu_image'/></Link>
                 <div id="navbar_options">
-                    {token &&
+                    {authed &&
                         <Link to="/track">
                             Track
                         </Link>
@@ -25,7 +56,7 @@ const Navbar = ()=>{
                         About
                     </Link>
                 </div>
-                {token == null ?
+                {authed == null ?
                     <div id='loginsignuup'>
                         <Login/>
                         <Button id='signup'>Signup</Button>
